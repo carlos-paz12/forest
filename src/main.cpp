4f538f2c2fd86696b48cd1fdcc12b10a44554d09
carlos-paz12 <capz.devel@gmail.com>
2025-06-16 16:46:19 -0300

    hard add nlohmann/json library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
+
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 int main() {
   std::cout << "Implementation of the AVL and Black-Red trees.\n";
+
+  json j;
+  std::ifstream ifs("database/users.json");
+  ifs >> j;
+
+  std::ofstream ofs("pretty.json");
+  ofs << std::setw(4) << j << std::endl;
+
+  std::cout << j;
+
   return EXIT_SUCCESS;
 }
